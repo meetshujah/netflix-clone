11a01bf6f8a32690bbdaaba5ed6f7d779c73bce4
Shuja Uddin <suddin@contoursoftware.net>
2020-10-09 21:14:57 +0500

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React,{useEffect,useState} from 'react';
+import axios from './axios';
+import './Nav.css';
+function Nav() {
+    const [show,handleShow]=useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll",()=>{
+            if(window.screenY>100){
+                handleShow(true)
+            }
+            else{
+                handleShow(false)
+            }
+            return()=>{
+                window.removeEventListener("scroll");
+            }
+        })
+    }, [])
+    return (
+        <nav className={`nav ${show && "nav__black"}`}>
+            <img 
+            className="nav__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/150px-Netflix_2015_logo.svg.png"
+            alt="Netflix"
+            />
+            <img 
+            className="nav__avatar"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/150px-Netflix_2015_logo.svg.png"
+            alt="Netflix"
+            />
+        </nav>
+    )
+}
+
+export default Nav
